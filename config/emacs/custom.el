d0457e8db0a449398a5dd4f763b99f934b194c01
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2021-12-28 10:44:18 +0100

    Increase font size slightly

--- config/emacs/custom.el
+++ config/emacs/custom.el
@@ -9,7 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 128 :foundry "SRC" :family "Hack"))))
+ '(default ((t (:height 116 :foundry "SRC" :family "Hack"))))
  '(haskell-keyword-face ((t (:inherit font-lock-keyword-face))))
  '(haskell-operator-face ((t (:inherit font-lock-keyword-face))))
  '(whitespace-tab ((t (:foreground "#636363")))))
