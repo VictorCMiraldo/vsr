f23fbe3f506082940f9e432eee9e721910c398a5
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2022-02-19 17:43:15 +0100

    Finally managed to add a nixpkgs channel... oh boy!

--- config/nix/install.sh
+++ config/nix/install.sh
@@ -21,3 +21,12 @@ sudo chmod 644 /etc/nix/nix.conf
 
 # Finally, we must restart the nix-daemon for changes to take effect.
 sudo systemctl restart nix-daemon
+
+# Add a nixpkgs channel
+echo "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.11.tar.gz nixpkgs" > ~/.nix-channels
+nix-channel --update
+
+# Now we install nix-direnv and configure it.
+# https://github.com/nix-community/nix-direnv#with-nix-env
+nix-env -f '<nixpkgs>' -iA nix-direnv
+echo "source $HOME/.nix-profile/share/nix-direnv/direnvrc" > $HOME/.direnvrc
