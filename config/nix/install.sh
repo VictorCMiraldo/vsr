3ac8963e23d0f9c04b501d7219985f011ceb0025
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2022-03-06 19:07:58 +0100

    Add ufw to list of packages to install; make sshd run on custom port. Split nix into install and post-install and add nix configuration to our bashrc

--- config/nix/install.sh
+++ config/nix/install.sh
@@ -15,12 +15,3 @@ fi
 # Download and install nix in single-user mode; multi user is giving me way too much trouble.
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
-# Link our nix.conf file and change its settings accordingly
-sudo mkdir -p /etc/nix
-sudo ln -fs "$(pwd)/nix.conf" /etc/nix/nix.conf
-sudo chmod 644 /etc/nix/nix.conf
-
-# Now we install nix-direnv and configure it.
-# https://github.com/nix-community/nix-direnv#with-nix-env
-nix-env -f '<nixpkgs>' -iA nix-direnv
-echo "source $HOME/.nix-profile/share/nix-direnv/direnvrc" > $HOME/.direnvrc
