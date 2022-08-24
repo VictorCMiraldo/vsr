bfb61e9e845d8d71824b253b49d577079539470f
Victor Miraldo <victor.miraldo@fastmail.com>
2022-08-24 16:52:17 +0200

    Change to system emacs

--- home-manager/programs/emacs/config.nix
+++ home-manager/programs/emacs/config.nix
@@ -1,10 +1,6 @@
 {pkgs, ...}:
 {
-  programs.emacs = {
-    enable = true;
-    package = pkgs.emacs28NativeComp;
-  };
-
+  # We won't rely on home-manager's emacs. Let's keep the system one.
   home.file.".emacs.d/init.el".source = ./init.el;
   home.file.".emacs.d/early-init.el".source = ./early-init.el;
   home.file.".emacs.d/custom.el".source = ./custom.el;
