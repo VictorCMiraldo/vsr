17016aefeddd71a2e412e364dd60e55beaba3f2a
Victor Miraldo <victor.miraldo@fastmail.com>
2022-09-06 14:30:17 +0200

    Emacs through nix seems fine

--- home-manager/programs/emacs/config.nix
+++ home-manager/programs/emacs/config.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}:
 {
-  # We won't rely on home-manager's emacs. Let's keep the system one.
+  programs.emacs.enable = true;
   home.file.".emacs.d/init.el".source = ./init.el;
   home.file.".emacs.d/early-init.el".source = ./early-init.el;
   home.file.".emacs.d/custom.el".source = ./custom.el;
