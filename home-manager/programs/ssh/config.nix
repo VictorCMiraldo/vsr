87f94cd063c9128b209d796c022dbc4ef40f324d
Victor Miraldo <victor.miraldo@fastmail.com>
2024-11-18 10:13:50 +0100

    Updates: decomissioning dev-lt-60; more outputs for sway; hacking unison until it works

--- home-manager/programs/ssh/config.nix
+++ home-manager/programs/ssh/config.nix
@@ -16,7 +16,6 @@
     home.file.".ssh/authorized_keys".text = ''
       ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHSwnBiaBmNkqUJyRfOjnE0rOwb80Bgkko149d/R/pXW victor@beetroot
       ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO40gphiJ99S0qmbYqLagjuUf9+x7+6khz5CvZe2MpjO victor@blackbean
-      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOI2iuzx7gQAuxi03FGgo4hrCy897X4zUjBV7Vtg+1Ue victor@dev-lt-60
       ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAW1EheUQ22usnWqIRr0pUlE5QhDFO9mH1nG0JIYgkKo victor@dev-lt-111
       '';
 
