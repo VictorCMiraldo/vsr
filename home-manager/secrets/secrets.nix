1904415619b7f9209ec03123f897e904b089c1d3
Victor Miraldo <victor.miraldo@fastmail.com>
2025-11-15 14:47:28 +0100

    Add instructions for how to edit secrets and add a secret

--- home-manager/secrets/secrets.nix
+++ home-manager/secrets/secrets.nix
@@ -1,6 +1,11 @@
 let
-  yourPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDKmZDkC1AJ2wiOnWlQZ68qhcX5+BqdvdAoo5Lyor2FZ";
+  # This is the public-key from ~/keychain/vsr-secrets/id_ed25519; editing secrets will require
+  # you to pass the identity file. So, to edit work-servers-data.age, the command is:
+  #
+  # > agenix -e work-servers-data.age -i ~/keychain/vsr-secrets/id_ed25519
+  #
+  pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDKmZDkC1AJ2wiOnWlQZ68qhcX5+BqdvdAoo5Lyor2FZ";
 in
 {
-  "work-servers-data.age".publicKeys = [ yourPublicKey ];
+  "work-servers-data.age".publicKeys = [ pubkey ];
 }
